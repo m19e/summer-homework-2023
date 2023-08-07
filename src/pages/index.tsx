c41d4cecb8ef73be11fbf1157c64ddb4c4182444
m19e <nexus.me7290@gmail.com>
2023-08-08 01:40:27 +0900

    feat: load google fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import type { NextPage, GetServerSideProps } from "next"
+import type { NextPage } from "next"
 
-import { Themes } from "components/templates/Themes"
+import { M_PLUS_Rounded_1c, Slackside_One } from "next/font/google"
+
+const fontRounded = M_PLUS_Rounded_1c({
+  weight: ["400"],
+  display: "swap",
+  preload: false,
+})
+
+const fontSlackside = Slackside_One({
+  weight: ["400"],
+  display: "swap",
+  preload: false,
+})
 
 const Page: NextPage = () => {
-  return <Themes />
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {},
-  }
+  return (
+    <main>
+      <h1 className={fontRounded.className}>
+        M+ Rounded 1c えむぷらすらんでぃどわんしー
+      </h1>
+      <p className={fontSlackside.className}>
+        Slackside One すらっくさいどわん
+      </p>
+    </main>
+  )
 }
 
 export default Page
